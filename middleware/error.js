3be18f1ca7548f8fac513f8abf16a16a4917df02
Nitish <nitishmankeshvar7@gmail.com>
2020-03-16 21:06:59 +0530

    Added Slugify and a slug to bootcamp schema

--- middleware/error.js
+++ middleware/error.js
@@ -11,7 +11,7 @@ const errorHandler = (err, req, res, next) => {
 
   if (err.code === 11000) {
     const message = `Cannot create dusplicate resource`;
-    error = new ErrorResponse(message, 400);
+    error = new ErrorResponse(messa00ge, 400);
   }
 
   if (err.name === 'ValidationError') {
